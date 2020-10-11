f4f6f8bd9c9bb0b16ef7d74a38163b33c45403fa
zhangyiteng <zhangyiteng@zhangyitengdeiMac.local>
2020-10-11 23:55:15 +0800

    update torchplus

---
+++ error_Torch.py
@@ -0,0 +1,148 @@
+@return_tensor_wrapper
+def T(self, *args, **kwargs):
+    return super().T(*args, **kwargs)
+
+@return_tensor_wrapper
+def as_strided_(self, *args, **kwargs):
+    return super().as_strided_(*args, **kwargs)
+
+@return_tensor_wrapper
+def clamp_max(self, *args, **kwargs):
+    return super().clamp_max(*args, **kwargs)
+
+@return_tensor_wrapper
+def clamp_max_(self, *args, **kwargs):
+    return super().clamp_max_(*args, **kwargs)
+
+@return_tensor_wrapper
+def clamp_min(self, *args, **kwargs):
+    return super().clamp_min(*args, **kwargs)
+
+@return_tensor_wrapper
+def clamp_min_(self, *args, **kwargs):
+    return super().clamp_min_(*args, **kwargs)
+
+@return_tensor_wrapper
+def coalesce(self, *args, **kwargs):
+    return super().coalesce(*args, **kwargs)
+
+@return_tensor_wrapper
+def data(self, *args, **kwargs):
+    return super().data(*args, **kwargs)
+
+@return_tensor_wrapper
+def device(self, *args, **kwargs):
+    return super().device(*args, **kwargs)
+
+@return_tensor_wrapper
+def dtype(self, *args, **kwargs):
+    return super().dtype(*args, **kwargs)
+
+@return_tensor_wrapper
+def grad(self, *args, **kwargs):
+    return super().grad(*args, **kwargs)
+
+@return_tensor_wrapper
+def grad_fn(self, *args, **kwargs):
+    return super().grad_fn(*args, **kwargs)
+
+@return_tensor_wrapper
+def imag(self, *args, **kwargs):
+    return super().imag(*args, **kwargs)
+
+@return_tensor_wrapper
+def is_coalesced(self, *args, **kwargs):
+    return super().is_coalesced(*args, **kwargs)
+
+@return_tensor_wrapper
+def is_distributed(self, *args, **kwargs):
+    return super().is_distributed(*args, **kwargs)
+
+@return_tensor_wrapper
+def is_nonzero(self, *args, **kwargs):
+    return super().is_nonzero(*args, **kwargs)
+
+@return_tensor_wrapper
+def is_same_size(self, *args, **kwargs):
+    return super().is_same_size(*args, **kwargs)
+
+@return_tensor_wrapper
+def layout(self, *args, **kwargs):
+    return super().layout(*args, **kwargs)
+
+@return_tensor_wrapper
+def log_softmax(self, *args, **kwargs):
+    return super().log_softmax(*args, **kwargs)
+
+@return_tensor_wrapper
+def map2_(self, *args, **kwargs):
+    return super().map2_(*args, **kwargs)
+
+@return_tensor_wrapper
+def name(self, *args, **kwargs):
+    return super().name(*args, **kwargs)
+
+@return_tensor_wrapper
+def new(self, *args, **kwargs):
+    return super().new(*args, **kwargs)
+
+@return_tensor_wrapper
+def prelu(self, *args, **kwargs):
+    return super().prelu(*args, **kwargs)
+
+@return_tensor_wrapper
+def real(self, *args, **kwargs):
+    return super().real(*args, **kwargs)
+
+@return_tensor_wrapper
+def reinforce(self, *args, **kwargs):
+    return super().reinforce(*args, **kwargs)
+
+@return_tensor_wrapper
+def relu(self, *args, **kwargs):
+    return super().relu(*args, **kwargs)
+
+@return_tensor_wrapper
+def relu_(self, *args, **kwargs):
+    return super().relu_(*args, **kwargs)
+
+@return_tensor_wrapper
+def resize(self, *args, **kwargs):
+    return super().resize(*args, **kwargs)
+
+@return_tensor_wrapper
+def resize_as(self, *args, **kwargs):
+    return super().resize_as(*args, **kwargs)
+
+@return_tensor_wrapper
+def shape(self, *args, **kwargs):
+    return super().shape(*args, **kwargs)
+
+@return_tensor_wrapper
+def smm(self, *args, **kwargs):
+    return super().smm(*args, **kwargs)
+
+@return_tensor_wrapper
+def softmax(self, *args, **kwargs):
+    return super().softmax(*args, **kwargs)
+
+@return_tensor_wrapper
+def sparse_resize_(self, *args, **kwargs):
+    return super().sparse_resize_(*args, **kwargs)
+
+@return_tensor_wrapper
+def sparse_resize_and_clear_(self, *args, **kwargs):
+    return super().sparse_resize_and_clear_(*args, **kwargs)
+
+@return_tensor_wrapper
+def split_with_sizes(self, *args, **kwargs):
+    return super().split_with_sizes(*args, **kwargs)
+
+@return_tensor_wrapper
+def sspaddmm(self, *args, **kwargs):
+    return super().sspaddmm(*args, **kwargs)
+
+@return_tensor_wrapper
+def to_dense(self, *args, **kwargs):
+    return super().to_dense(*args, **kwargs)
+
