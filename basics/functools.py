5dc1cfaf8d083d996d31313de85d6b57ea3a42d1
Bertie <2247501256@qq.com>
2020-10-15 23:59:12 +0800

    Update functools.py

--- basics/functools.py
+++ basics/functools.py
@@ -11,4 +11,5 @@ def get_environ_vars():
     while "pyctlib" in str(frame.f_code): frame = frame.f_back
     all_vars = frame.f_globals.copy()
     all_vars.update(frame.f_locals)
-    return all_vars
+    frame.f_locals.update(all_vars)
+    return frame.f_locals
