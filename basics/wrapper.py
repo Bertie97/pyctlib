cbca2d81580b9038edddf30ac4e9deb9a03f4df0
Bertie <2247501256@qq.com>
2020-10-15 13:17:50 +0800

    Update wrapper.py

--- basics/wrapper.py
+++ basics/wrapper.py
@@ -32,21 +32,26 @@ class return_type_wrapper:
 
 def decorator(*wrapper_func, use_raw = True):
     if len(wrapper_func) > 2: raise TypeError("Too many arguments for @decorator")
-    elif len(wrapper_func) == 1: wrapper_func = wrapper_func[0]
-    else: return lambda x: decorator(x, use_raw = use_raw)
+    elif len(wrapper_func) == 1: wrapper_func = wrapper_func[0]; ret = None
+    else: ret = lambda x: decorator(x, use_raw = use_raw)
     if not isinstance(wrapper_func, type(decorator)): raise TypeError("@decorator wrapping a non-wrapper")
-    def wrapper(*args, **kwargs):
-        if not kwargs and len(args) == 1:
-            func = args[0]
-            raw_func = raw_function(func)
-            if isinstance(raw_func, type(decorator)):
-                func_name = f"{raw_func.__name__}[{wrapper_func.__qualname__.split('.')[0]}]"
-                wrapped_func = wraps(raw_func)(wrapper_func(raw_func if use_raw else func))
-                wrapped_func.__name__ = func_name
-                wrapped_func.__doc__ = raw_func.__doc__
-                environ_vars = get_environ_vars()
-                environ_vars[raw_func.__name__] = wrapped_func
-                exec(f"def {raw_func.__name__}(): ...")
-                return eval(f"{raw_func.__name__}")
-        return decorator(wrapper_func(*args, **kwargs))
-    return wraps(wrapper_func)(wrapper)
+    if not ret:
+        def wrapper(*args, **kwargs):
+            if not kwargs and len(args) == 1:
+                func = args[0]
+                raw_func = raw_function(func)
+                if isinstance(raw_func, type(decorator)):
+                    func_name = f"{raw_func.__name__}[{wrapper_func.__qualname__.split('.')[0]}]"
+                    wrapped_func = wraps(raw_func)(wrapper_func(raw_func if use_raw else func))
+                    wrapped_func.__name__ = func_name
+                    wrapped_func.__doc__ = raw_func.__doc__
+                    environ_vars = get_environ_vars()
+                    environ_vars[raw_func.__name__] = wrapped_func
+                    exec(f"def {raw_func.__name__}(): ...")
+                    return eval(f"{raw_func.__name__}")
+            return decorator(wrapper_func(*args, **kwargs))
+        ret = wraps(wrapper_func)(wrapper)
+    environ_vars = get_environ_vars()
+    environ_vars[wrapper_func.__name__] = ret
+    exec(f"def {wrapper_func.__name__}(): ...")
+    return eval(f"{wrapper_func.__name__}")
