f8d20d8eac360500f896567cb74317bf16e1a737
张翼腾 <zhangyiteng@zhangyitengdeMacBook-Pro.local>
2020-10-19 15:18:42 +0800

    bug fix

--- torchplus/nn/modules/modules.py
+++ torchplus/nn/modules/modules.py
@@ -150,27 +150,4 @@ class Module(nn.Module):
 
         return self
 
-class Linear(Module):
-
-    def __init__(self, in_features: int, out_features: int, bias: bool = True):
-
-        super(Linear, self).__init__()
-        self.in_features = in_features
-        self.out_feature = out_features
-        self.weight = Parameter(torchplus.zeros(in_features, out_features))
-        if bias:
-            self.bias = Parameter(torchplus.zeros(out_features))
-        else:
-            self.bias = None
-
-    def forward(self, x):
-        if self.bias is not None:
-            return x @ self.weight + self.bias.unsqueeze(0)
-        else:
-            return x @ self.weight
-
-    def __str__(self):
-        return "Linear(in_features={}, out_features={}, bias={})".format(self.in_features, self.out_feature, self.bias is not None)
-
-    def __repr__(self):
-        return str(self)
+__all__ = ["Module"]
