0c0015dca04e8f9f7390ea1e5d997bb13a4dd20f
Bertie <2247501256@qq.com>
2020-10-19 16:17:51 +0800

    Update func_tools.py

--- basics/func_tools.py
+++ basics/func_tools.py
@@ -8,7 +8,7 @@ import sys
 
 def get_environ_vars():
     frame = sys._getframe()
-    while "pyctlib" in str(frame.f_code): frame = frame.f_back
+    while "pyctlib" in str(frame.f_code) or 'file "<' in str(frame.f_code): frame = frame.f_back
     all_vars = frame.f_globals.copy()
     all_vars.update(frame.f_locals)
     frame.f_locals.update(all_vars)
