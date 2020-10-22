c039012afc97963408e475a0685c7f118baceff9
Bertie <2247501256@qq.com>
2020-10-23 01:16:09 +0800

    nothing

--- setup_torchplus.py
+++ setup_torchplus.py
@@ -10,11 +10,13 @@
 from setuptools import setup, find_packages
  
 setup(
-    name = "pyctlib",
+    name = "torchplus",
     version = "0.1.0",
-    keywords = ("pip", "pyctlib", "overload"),
+    keywords = ("pip", "pyctlib", "torchplus"),
     description = "This package is based on pytorch and try to provide a more user-friendly interface for pytorch",
-    long_description = "We encapsulated a new type on top of torch.Tenser, which we also call it Tensor. It has the same function as torch.Tensor, but it can change to cuda device automatically. Also, we try to provide more useful module for torch users to make deep learning earier to be implemented.",
+    # long_description = "We encapsulated a new type on top of torch.Tenser, which we also call it Tensor. It has the same function as torch.Tensor, but it can change to cuda device automatically. Also, we try to provide more useful module for torch users to make deep learning earier to be implemented.",
+    long_description = open("./torchplus/README.md").read(),
+    long_description_content_type="text/markdown",
     license = "MIT Licence",
  
     url = "https://github.com/jiangfubang/balabala",       # 项目相关文件地址，一般是github，有没有都行吧
