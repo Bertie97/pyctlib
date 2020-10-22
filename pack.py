1d3c7f15ae6e81d552ec7fcb2af6457fa4967627
Bertie <2247501256@qq.com>
2020-10-23 00:48:47 +0800

    new setup

--- pack.py
+++ pack.py
@@ -8,9 +8,10 @@
 
 import sys, os
 from pyinout import path
+from pyctlib import vector
 
 assert len(sys.argv) > 1
 package_name = sys.argv[1]
 
 os.system(f"python3 setup_{package_name}.py sdist --dist-dir dist_{package_name}")
-os.system(f"twine upload {str(sorted(path('dist_' + package_name))[-1])}")
+os.system(f"twine upload {str(vector(path('dist_' + package_name)).max())}")
--- setup_pyoverload.py
+++ setup_pyoverload.py
@@ -6,15 +6,21 @@
 @Contact    :   2247501256@qq.com
 @Dect       :   None
 '''
- 
+
+import re
 from setuptools import setup, find_packages
+
+def wrap_code_fences(file_str):
+    for code in re.findall(r"```[^(```)]+```", file_str):
+        file_str = file_str.replace(code, '\n'.join(['\t' + l for l in code.split('\n') if '```' not in l]))
+    return file_str
  
 setup(
     name = "pyoverload",
-    version = "0.5.2",
+    version = "0.5.3",
     keywords = ("pip", "pyctlib", "overload"),
     description = "'pyoverload' overloads the functions by simply using typehints and adding decorator '@overload'.",
-    long_description = open("./pyoverload/README.md").read(),
+    long_description = wrap_code_fences(open("./pyoverload/README.md").read()),
     license = "MIT Licence",
  
     url = "https://github.com/Bertie97/pyctlib/tree/main/pyoverload",
