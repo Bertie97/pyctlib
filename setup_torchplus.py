0dcc8f646f1e47c7271a5780c157de3f4442fb92
zhangyiteng <zhangyiteng@zhangyitengdeiMac.local>
2020-10-23 09:57:05 +0800

    update

--- setup_torchplus.py
+++ setup_torchplus.py
@@ -14,12 +14,12 @@ setup(
     version = "0.1.0",
     keywords = ("pip", "pyctlib", "torchplus"),
     description = "This package is based on pytorch and try to provide a more user-friendly interface for pytorch",
-    # long_description = "We encapsulated a new type on top of torch.Tenser, which we also call it Tensor. It has the same function as torch.Tensor, but it can change to cuda device automatically. Also, we try to provide more useful module for torch users to make deep learning earier to be implemented.",
-    long_description = open("./torchplus/README.md").read(),
-    long_description_content_type="text/markdown",
+    long_description = "We encapsulated a new type on top of torch.Tenser, which we also call it Tensor. It has the same function as torch.Tensor, but it can change to cuda device automatically. Also, we try to provide more useful module for torch users to make deep learning earier to be implemented.",
+    # long_description = open("./torchplus/README.md").read(),
+    # long_description_content_type="text/markdown",
     license = "MIT Licence",
  
-    url = "https://github.com/jiangfubang/balabala",       # 项目相关文件地址，一般是github，有没有都行吧
+    url = "https://github.com/Bertie97/pyctlib/tree/main/torchplus",       # 项目相关文件地址，一般是github，有没有都行吧
     author = "Zhang Yiteng & Zhou Yuncheng",
     author_email = "zytfdu@icloud.com",
  
