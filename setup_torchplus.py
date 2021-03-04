5dacbad0060951f0d79ed66eee9f6b3c009752f1
张翼腾 <zhangyiteng@zhangyitengdeMacBook-Pro-2.local>
2021-03-04 11:48:11 +0800

    update version number

--- setup_torchplus.py
+++ setup_torchplus.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
  
 setup(
     name = "torchplus",
-    version = "0.2.30",
+    version = "0.2.32",
     keywords = ("pip", "pyctlib", "torchplus"),
     description = "This package is based on pytorch and try to provide a more user-friendly interface for pytorch",
     long_description = open("./torchplus/README.md").read(),
