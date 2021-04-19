94d40ede1abdf46738373f32d6e223ee7c73477c
Your Name <you@example.com>
2021-04-19 16:21:31 +0800

    update

--- setup_tpdataset.py
+++ setup_tpdataset.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
  
 setup(
     name = "tpdataset",
-    version = "0.0.5",
+    version = "0.0.6",
     keywords = ("pip", "pyctlib"),
     description = "This is A foundamental package containing some basic self-designed functions and types for Project PyCTLib. ",
     long_description = open("./pyctlib/README.md").read(),
@@ -25,5 +25,5 @@ setup(
     packages = find_packages(),
     include_package_data = True,
     platforms = "any",
-    install_requires = ["pyctlib", "torchplus"]
+    install_requires = ["pyctlib", "torchplus", "requests"]
 )
