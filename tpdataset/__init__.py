099e977dcdc4bf6b8e2a998e279fff561b7688e4
Your Name <you@example.com>
2021-04-19 16:19:40 +0800

    update

--- tpdataset/__init__.py
+++ tpdataset/__init__.py
@@ -1,3 +1,4 @@
 # This packge contain common dataset used by Machine Learning which provide simple and consistent experience
 
 from .raw_dataset import RawDataSet, DataDownloader
+from . import NLP
