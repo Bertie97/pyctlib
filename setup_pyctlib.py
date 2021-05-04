36a81bfaf978e268c3c9373851a43abb2f4354a3
zhangyiteng <zhangyiteng@zhangyitengdeiMac.local>
2021-05-04 12:04:13 +0800

    bug fix

--- setup_pyctlib.py
+++ setup_pyctlib.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
  
 setup(
     name = "pyctlib",
-    version = "0.2.117",
+    version = "0.2.118",
     keywords = ("pip", "pyctlib"),
     description = "This is A foundamental package containing some basic self-designed functions and types for Project PyCTLib. ",
     long_description = open("./pyctlib/README.md").read(),
