ae95af6b130e0d4ba2a77cbc09e368441a3b5c28
Your Name <you@example.com>
2021-05-08 11:19:05 +0800

    update: add smooth

--- pyctlib/vector.py
+++ pyctlib/vector.py
@@ -1688,6 +1688,61 @@ class vector(list):
             return vector(numba_cumsum(np.array(self)))
         return self.cumulative_reduce(lambda x, y: x + y)
 
+    def smooth(self, window_len=11, window='hanning'):
+        """smooth the data using a window with requested size.
+
+        This method is based on the convolution of a scaled window with the signal.
+        The signal is prepared by introducing reflected copies of the signal
+        (with the window size) in both ends so that transient parts are minimized
+        in the begining and end part of the output signal.
+
+        input:
+            x: the input signal
+            window_len: the dimension of the smoothing window; should be an odd integer
+            window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+                flat window will produce a moving average smoothing.
+
+        output:
+            the smoothed signal
+
+        example:
+
+        t=linspace(-2,2,0.1)
+        x=sin(t)+randn(len(t))*0.1
+        y=smooth(x)
+
+        see also:
+
+        numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+
+        TODO: the window parameter could be the window itself if an array instead of a string
+        NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+        """
+
+
+        if self.length < window_len:
+            raise ValueError("Input vector needs to be bigger than window size.")
+
+        if not self.check_type(int) and not self.check_type(float):
+            raise ValueError("smooth only accepts 1 dimension arrays.")
+
+        if window_len<3:
+            return self
+
+        if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+            raise ValueError("Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'")
+
+        x = np.array(self)
+        s = np.r_[x[window_len - 1:0:-1], x, x[-2:-window_len - 1:-1]]
+        #print(len(s))
+        if window == 'flat': #moving average
+            w = np.ones(window_len, 'd')
+        else:
+            w = np.__getattribute__(window)(window_len)
+
+        y = np.convolve(w / w.sum(), s, mode='valid')
+        return vector(y)
+
     def norm(self, p=2):
         """
         norm of vector
