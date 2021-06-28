26a58c887d4027a6091b293ff91af00ba5a236a8
sillybun <zhangytfudan@gmail.com>
2021-06-28 10:18:42 +0800

    update

--- torchfunction/optim/lr_scheduler.py
+++ torchfunction/optim/lr_scheduler.py
@@ -7,7 +7,9 @@ from torch import nn
 from torch.optim import Adam, Optimizer
 from torch.optim.lr_scheduler import _LRScheduler
 import math
-from torch.optim.lr_scheduler import StepLR, ExponentialLR
+from torch.optim.lr_scheduler import StepLR, ExponentialLR, CosineAnnealingLR
+from pyctlib import Logger
+logger = Logger(True)
 
 class _enable_get_lr_call:
 
@@ -37,22 +39,33 @@ class LinearWarmupLRWrapper(_LRScheduler):
 
     def __init__(self, lr_scheduler: _LRScheduler, warmup_epochs: int):
 
+        last_epoch = lr_scheduler.last_epoch
         self.lr_scheduler = lr_scheduler
         self.warmup_epochs = warmup_epochs
         super(LinearWarmupLRWrapper, self).__init__(lr_scheduler.optimizer, lr_scheduler.last_epoch, verbose=lr_scheduler.verbose)
+        self.last_epoch = last_epoch
+        temp = self
+        while hasattr(temp, "lr_scheduler"):
+            temp.lr_scheduler.last_epoch = last_epoch
+            temp = temp.lr_scheduler
 
     def get_lr(self):
         return self._get_closed_form_lr()
 
     def _get_closed_form_lr(self):
         lr_value = self.lr_scheduler._get_closed_form_lr()
+        logger.variable("cos_lr", lr_value[0])
+        logger.info("output warmup", lr_value[0] * self.last_epoch / self.warmup_epochs)
         if self.last_epoch < self.warmup_epochs:
             return [lr * self.last_epoch / self.warmup_epochs for lr in lr_value]
         else:
             return lr_value
 
     def step(self):
-        self.lr_scheduler.last_epoch = self.last_epoch
+        temp = self
+        while hasattr(temp, "lr_scheduler"):
+            temp.lr_scheduler.last_epoch = self.last_epoch + 1
+            temp = temp.lr_scheduler
         super(LinearWarmupLRWrapper, self).step()
 
 class ExponentialDecayLRWrapper(_LRScheduler):
@@ -60,21 +73,31 @@ class ExponentialDecayLRWrapper(_LRScheduler):
     def __init__(self, lr_scheduler: _LRScheduler, max_epochs: int, total_decay: float):
 
         assert total_decay <= 1
+        last_epoch = lr_scheduler.last_epoch
         self.lr_scheduler = lr_scheduler
         self.max_epochs = max_epochs
         self.total_decay = total_decay
         self.gamma = math.log(total_decay) / max_epochs
         super(ExponentialDecayLRWrapper, self).__init__(lr_scheduler.optimizer, lr_scheduler.last_epoch, verbose=lr_scheduler.verbose)
+        self.last_epoch = last_epoch
+        temp = self
+        while hasattr(temp, "lr_scheduler"):
+            temp.lr_scheduler.last_epoch = last_epoch
+            temp = temp.lr_scheduler
 
     def _get_closed_form_lr(self):
         lr_value = self.lr_scheduler._get_closed_form_lr()
+        logger.variable("warmup_lr", lr_value)
         return [lr * math.exp(self.last_epoch * self.gamma) for lr in lr_value]
 
     def get_lr(self):
         return self._get_closed_form_lr()
 
     def step(self):
-        self.lr_scheduler.last_epoch = self.last_epoch
+        temp = self
+        while hasattr(temp, "lr_scheduler"):
+            temp.lr_scheduler.last_epoch = self.last_epoch + 1
+            temp = temp.lr_scheduler
         super(ExponentialDecayLRWrapper, self).step()
 
 if True:
@@ -84,7 +107,8 @@ if True:
     optim = SGD(model, 0.1)
 
     # scheduler_warmup is chained with schduler_steplr
-    scheduler_steplr = ExponentialDecayLRWrapper(LinearWarmupLRWrapper(ConstantLR(optim), 10), 20, 0.1)
+    scheduler_steplr = ExponentialDecayLRWrapper(LinearWarmupLRWrapper(CosineAnnealingLR(optim, T_max=20), 10), 100, 0.1)
+    # scheduler_steplr = ExponentialDecayLRWrapper(ConstantLR(optim), 10, 0.1)
     # scheduler_warmup = GradualWarmupScheduler(optim, multiplier=1, total_epoch=5, after_scheduler=scheduler_steplr)
 
     # this zero gradient update is needed to avoid a warning message, issue #8.
@@ -92,9 +116,10 @@ if True:
     optim.step()
     l_v = vector()
 
-    for epoch in range(1, 20):
+    for epoch in range(0, 100):
         print(epoch, optim.param_groups[0]['lr'])
         l_v.append(optim.param_groups[0]['lr'])
 
         optim.step()    # backward pass (update network)
         scheduler_steplr.step()
+        # print(scheduler_steplr.last_epoch, "?")
